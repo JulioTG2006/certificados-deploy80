d58724457e3b2b864a068a410af5114e89f18219
Julio Torres <julio.torres.ga@tecsup.edu.pe>
2026-06-04 14:05:37 -0500

    fix mysql railway connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,12 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
 import { config } from "./env.js";
 
-dotenv.config();
-
 export const getConnection = async () => {
-  return await mysql.createConnection({
+  return mysql.createConnection({
     host: config.db.host,
     user: config.db.user,
     password: config.db.password,
     database: config.db.name,
+    port: config.db.port,
   });
 };
