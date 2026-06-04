5787db58366db21a653a44898d36765f25466787
Julio Torres <julio.torres.ga@tecsup.edu.pe>
2026-06-04 14:27:56 -0500

    remove debug logs

--- backend/app.js
+++ backend/app.js
@@ -31,10 +31,7 @@ app.use(
   "/api/certificados",
   certificadosRoutes
 );
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_PORT:", process.env.DB_PORT);
+
 
 /* 🔥 TEST DB */
 const testDB = async () => {
